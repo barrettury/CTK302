0b877836e2d62915c1a674b7280365d03f79986c
barrettury <58411313+barrettury@users.noreply.github.com>
2020-09-03 09:28:26 -0500

    Add files via upload

--- p5/Unit1/fonts/sketch.js
+++ p5/Unit1/fonts/sketch.js
@@ -1,7 +1,9 @@
 var f;
+var p2;
 function setup() {
 createCanvas(800,800);
 f = loadFont("Jumpman.ttf");
+p2 = loadFont("PressStart2P.ttf");
 textAlign(CENTER);
 }
 
@@ -10,5 +12,11 @@ background(100);
 textFont(f);
 textSize(68);
 text("King K. Rool", width/2,height/2);
+textFont(p2)
+textSize(30);
+text("This is", width/2, 500);
+text("MY personal", width/2, 600);
+text("Font!", width/2, 700);
+
 
 }
