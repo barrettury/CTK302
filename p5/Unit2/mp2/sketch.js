2b5708ad3168edbd9f1d4e16f997972063d23369
barrettury <58411313+barrettury@users.noreply.github.com>
2020-10-03 15:55:42 -0500

    Add files via upload

--- p5/Unit2/mp2/sketch.js
+++ p5/Unit2/mp2/sketch.js
@@ -44,7 +44,7 @@ function draw() {
       image(img1, 260, 300, 200, 200);
       image(img2, 100, 190, 250, 250);
       timer++;
-      if (timer > 325) {
+      if (timer > 300) {
         state++;
       }
       break;
