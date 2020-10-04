811c2f19670959cf8fabd62b93ec5f9b3bcc0587
barrettury <58411313+barrettury@users.noreply.github.com>
2020-10-04 15:59:23 -0500

    Add files via upload

--- p5/Unit2/mp2/sketch.js
+++ p5/Unit2/mp2/sketch.js
@@ -4,6 +4,9 @@ var s;
 var x = 0;
 var img1;
 var img2;
+var img3;
+var img4;
+var rwby;
 var pet = 0;
 var timer = 0;
 
@@ -15,6 +18,10 @@ function setup() {
   createCanvas(500, 500);
   img1 = loadImage("Zwei_Chibi.png");
   img2 = loadImage("RubywithChicken.png");
+  img3 = loadImage("WeissBlakeSit.png");
+  img4 = loadImage("sofa.png");
+  rwby = loadFont("rwby_style.ttf");
+
   imageMode(CENTER);
   textAlign(CENTER);
 }
@@ -22,11 +29,16 @@ function setup() {
 function draw() {
   background(240);
   numberOfTouches = touches.length;
-  text(numberOfTouches + ' touches', 30, 10);
+  text(numberOfTouches + ' touches', 40, 20);
 
+  textFont(rwby);
+  textSize(20);
+  image(img4,width/2, height/2,400,400);
+  image(img3,250,300,300,300);
   switch (state) {
     case 0:
-      image(img1, 260, 300, 200, 200);
+    text("Pet Zwei to start a cutscene",width/2,40);
+      image(img1, 260, 400, 200, 200);
       if ((numberOfTouches > 0) && (mouseX > 199) && (mouseX < 301) && (mouseY > 199) && (mouseY < 401)) {
         pet++;
       }
@@ -41,8 +53,8 @@ function draw() {
       break;
 
     case 2:
-      image(img1, 260, 300, 200, 200);
-      image(img2, 100, 190, 250, 250);
+      image(img1, 260, 400, 200, 200);
+      image(img2, 100, 290, 250, 250);
       timer++;
       if (timer > 300) {
         state++;
@@ -54,8 +66,8 @@ function draw() {
       if (x > -300) {
         push();
         translate(x, 0);
-        image(img1, 260, 300, 200, 200);
-        image(img2, 100, 190, 250, 250);
+        image(img1, 260, 400, 200, 200);
+        image(img2, 100, 290, 250, 250);
         pop();
       } else {
         state++;
