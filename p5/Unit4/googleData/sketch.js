365b8c9044e4712a2ccb3ee7e4a32e0c46c31bec
barrettury <58411313+barrettury@users.noreply.github.com>
2020-11-11 19:36:10 -0600

    Add files via upload

--- p5/Unit4/googleData/sketch.js
+++ p5/Unit4/googleData/sketch.js
@@ -14,9 +14,9 @@ var char1, char2, char3, char4, char5, char6, char7, char8;
 var p1, p2, p3, p4;
 var back;
 var first, second, third, pcFave;
-var monsterNames = ["Miranda", "Damien", "Scott", "Liam", "Polly", "Vera", "Zoe", "Calculester"]
+var monsterNames = ["Placeholder","Miranda", "Damien", "Scott", "Liam", "Polly", "Vera", "Zoe", "Calculester"]
 var monsterScores = [];
-var playerNames = ["Oz", "Amira", "Brian", "Vicky"];
+var playerNames = ["Placeholder","Oz", "Amira", "Brian", "Vicky"];
 var playerScores = [];
 let state = 0;
 
@@ -68,9 +68,48 @@ function gotData(data) {
 
   // iterate through the array of data and create an object and push it on an array called bubbles
   for (let i = 0; i < data.length; i++) {
-    bubbles.push(new Bubble(data[i].PC, data[i].WE, data[i].DDC, data[i].P, data[i].SMOOCH)); // THESE Name and Shape need to match your column names in your spreadsheet!
-  }
+    if (data[i].char == "Oz") oz++;
+    else if (data[i].char == "Amira") amira++;
+    else if (data[i].char == "Brian") brian++;
+    else if (data[i].char == "Vicky") vicky++;
 
+    if (data[i].world == "Nobody ends the world but me! I'll end the world today.") damien++;
+    else if (data[i].world == "I'll finish my novel... whoever comes after the end should know my legacy!") liam++;
+    else if (data[i].world == "It's okay! We invented the apocalypse to take care of the overpopulation of commoners.") miranda++;
+    else if (data[i].world == "I always party as if there were no tomorrow... so who cares?") polly++;
+    else if (data[i].world == "100 push-ups! No, no... 200 push-ups!") scott++;
+    else if (data[i].world == "They always tell you the world is ending... I'll profit on other people's hysteria.") vera++;
+    else if (data[i].world == "Netflix and chill.") zoe++;
+    else if (data[i].world == "I would panic, because that's what real organic people do and I am real organic person.") calcu++;
+
+    if (data[i].dnd == "Gun Haver") damien++;
+    else if (data[i].dnd == "Obscure Instrgrammer") liam++;
+    else if (data[i].dnd == "The Prettiest One") miranda++;
+    else if (data[i].dnd == "Partysmith") polly++;
+    else if (data[i].dnd == "Good Boy") scott++;
+    else if (data[i].dnd == "Moneymaker") vera++;
+    else if (data[i].dnd == "Fanfic Bard") zoe++;
+    else if (data[i].dnd == "Automation") calcu++;
+
+    if (data[i].prod == "Extra flammable.") damien++;
+    else if (data[i].prod == "You won't understand me, and that's how you know I'm cool.") liam++;
+    else if (data[i].prod == "I command you to enjoy me.") miranda++;
+    else if (data[i].prod == "Be ready for a long trip.") polly++;
+    else if (data[i].prod == "Buy me, I'm good.") scott++;
+    else if (data[i].prod == "Too expensive for you.") vera++;
+    else if (data[i].prod == "For fans by fans.") zoe++;
+    else if (data[i].prod == "Objectively better than the other options.") calcu++;
+
+    if (data[i].date == "Damien") damien += 2;
+    else if (data[i].date == "Liam") liam += 2;
+    else if (data[i].date == "Miranda") miranda += 2;
+    else if (data[i].date == "Polly") polly += 2;
+    else if (data[i].date == "Scott") scott += 2;
+    else if (data[i].date == "Vera") vera += 2;
+    else if (data[i].date == "Zoe") zoe += 2;
+    else if (data[i].date == "Calculester") calcu += 2;
+
+  }
 }
 
 
@@ -78,14 +117,7 @@ function draw() {
   switch (state) {
     case 0:
       // // iterate through the bubbles and display the objects!
-      for (let i = 0; i < bubbles.length; i++) {
-        bubbles[i].playerTally();
-        bubbles[i].monsterTally1();
-        bubbles[i].monsterTally2();
-        bubbles[i].monsterTally3();
-        bubbles[i].smoochTime();
 
-      }
       state = 1;
       break;
     case 1:
@@ -96,11 +128,11 @@ function draw() {
       textSize(10);
       text("There's no such thing as a bias, what are you talking about?", 230, 50);
       first = 0;
-      second = 1;
-      third = 2;
+      second = 0;
+      third = 0;
       pcFave = 0;
-      monsterScores = [miranda, damien, scott, liam, polly, vera, zoe, calcu];
-      playerScores = [oz, amira, brian, vicky];
+      monsterScores = [-1, miranda, damien, scott, liam, polly, vera, zoe, calcu];
+      playerScores = [-1, oz, amira, brian, vicky];
       for (let i = 0; i < playerScores.length; i++) {
         if (playerScores[pcFave] < playerScores[i]) pcFave = i;
       }
@@ -108,10 +140,10 @@ function draw() {
         if (monsterScores[first] < monsterScores[i]) first = i;
       }
       for (let i = 0; i < monsterScores.length; i++) {
-        if (monsterScores[second] < monsterScores[i] && !(second == first)) second = i;
+        if ((monsterScores[second] < monsterScores[i]) && !(i == first)) second = i;
       }
       for (let i = 0; i < monsterScores.length; i++) {
-        if (monsterScores[third] < monsterScores[i] && !(third == first) && !(third == first)) third = i;
+        if ((monsterScores[third] < monsterScores[i]) && !(i == first) && !(i == second)) third = i;
       }
       fill("gold");
       if (monsterNames[first] == "Miranda") {
@@ -219,69 +251,4 @@ function draw() {
       }
       break;
   }
-}
-
-
-
-
-// my Bubble class
-class Bubble {
-
-  constructor(myPlayer, myWorld, myDnd, myProd, myDate) {
-    this.char = myPlayer;
-    this.world = myWorld;
-    this.dnd = myDnd;
-    this.prod = myProd;
-    this.date = myDate;
-
-  }
-
-
-  playerTally() {
-    if (this.char == "Oz") oz++;
-    else if (this.char == "Amira") amira++;
-    else if (this.char == "Brian") brian++;
-    else if (this.char == "Vicky") vicky++;
-  }
-  monsterTally1() {
-    if (this.world == "Nobody ends the world but me! I'll end the world today.") damien++;
-    else if (this.world == "I'll finish my novel... whoever comes after the end should know my legacy!") liam++;
-    else if (this.world == "It's okay! We invented the apocalypse to take care of the overpopulation of commoners.") miranda++;
-    else if (this.world == "I always party as if there were no tomorrow... so who cares?") polly++;
-    else if (this.world == "100 push-ups! No, no... 200 push-ups!") scott++;
-    else if (this.world == "They always tell you the world is ending... I'll profit on other people's hysteria.") vera++;
-    else if (this.world == "Netflix and chill.") zoe++;
-    else if (this.world == "I would panic, because that's what real organic people do and I am real organic person.") calcu++;
-  }
-  monsterTally2() {
-    if (this.dnd == "Gun Haver") damien++;
-    else if (this.dnd == "Obscure Instrgrammer") liam++;
-    else if (this.dnd == "The Prettiest One") miranda++;
-    else if (this.dnd == "Partysmith") polly++;
-    else if (this.dnd == "Good Boy") scott++;
-    else if (this.dnd == "Moneymaker") vera++;
-    else if (this.dnd == "Fanfic Bard") zoe++;
-    else if (this.dnd == "Automation") calcu++;
-  }
-  monsterTally3() {
-    if (this.prod == "Extra flammable.") damien++;
-    else if (this.prod == "You won't understand me, and that's how you know I'm cool.") liam++;
-    else if (this.prod == "I command you to enjoy me.") miranda++;
-    else if (this.prod == "Be ready for a long trip.") polly++;
-    else if (this.prod == "Buy me, I'm good.") scott++;
-    else if (this.prod == "Too expensive for you.") vera++;
-    else if (this.prod == "For fans by fans.") zoe++;
-    else if (this.prod == "Objectively better than the other options.") calcu++;
-  }
-  smoochTime() {
-    if (this.dnd == "Damien") damien += 2;
-    else if (this.date == "Liam") liam += 2;
-    else if (this.date == "Miranda") miranda += 2;
-    else if (this.date == "Polly") polly += 2;
-    else if (this.date == "Scott") scott += 2;
-    else if (this.date == "Vera") vera += 2;
-    else if (this.date == "Zoe") zoe += 2;
-    else if (this.date == "Calculester") calcu += 2;
-  }
-
 }
